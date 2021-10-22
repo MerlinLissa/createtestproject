c0f77177ba921a1e7aedcec53727785477a9ace6
IlshatIbrahimov <ilshat.ibrahimov721@gmail.com>
2021-10-22 11:50:55 +0300

    init'

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace liza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Task
+    {
+        public static int Power(int number, int exponent)
+        {
+            int p = number;
+            for(int i = 1; i <= exponent; i++)
+            {
+                p = p * number;
+            }
+            return p;
+        }
+    }
+}
